65fc4c5000a476c35c0a6feef16ef2dce13e63c3
Anthea Middleton <antheamiddleton@shotscope.com>
2021-02-20 18:05:55 +0000

    Upload page and API functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../actions';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 import Button from '../components/Button';
 import '../App.css';
 // import { testFunction } from '../util';
@@ -12,19 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="topNav">
-              <h1 className="customHeader">
-                Beautiful Cats
-              </h1>
-              <div className="upload">
-                <Button text="UPLOAD NEW CAT" />
-              </div>
-            </div>
-            <div className="divider"></div>
-          </div>
-        </div>
+        <Header root />
       </div>
     )
   }
--- src/pages/upload.js
+++ src/pages/upload.js
@@ -1,20 +1,109 @@
-// Upload Cats
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../actions';
-import '../App.css';
+// import ReactCrop from "react-image-crop";
+import { compressToLimit, uploadImage } from '../util';
+import Header from '../components/Header';
+import defaultImg from '../assets/images/blankCat.png';
 
 class Upload extends Component {
 
-  render() {
-    return (
-        <div>
-          TEST
-        </div>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      fileToUpload: "",
+      loading: false,
+      imageSource: defaultImg,
+      saveButton: false
     }
   }
+
+  _onChange() {
+    // Preview the image
+    var file = this.refs.file.files[0];
+    // Check an image has been uploaded
+    if (this.refs.file.files.length > 0) {
+      var reader = new FileReader();
+      var url = reader.readAsDataURL(file);
+
+      reader.onloadend = function (e) {
+        this.setState({
+            imageSource: [reader.result],
+            saveButton: true,
+            fileToUpload: file
+        })
+       }.bind(this);
+    }
+  }
+
+  submit() {
+    // Ensure image is sized down
+    const formData = new FormData();
+    formData.append("file", this.state.fileToUpload);
+    formData.append("sub_id", this.state.fileToUpload.name);
+    var fileToStringify  = {
+     'lastModified'     : this.state.fileToUpload.lastModified,
+     'lastModifiedDate' : this.state.fileToUpload.lastModifiedDate,
+     'name'             : this.state.fileToUpload.name,
+     'size'             : this.state.fileToUpload.size,
+     'type'             : this.state.fileToUpload.type
+   };
+    const bodyToSend = {
+      "file": JSON.stringify(fileToStringify),
+      "sub_id": this.state.fileToUpload.name
+    }
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'x-api-key': '45d49036-1938-44e2-b443-af805aeb55fb'},
+        body: formData
+    };
+    fetch('https://api.thecatapi.com/v1/images/upload', requestOptions)
+        .then(response => response.json())
+        .then((responseJson)=> {
+          debugger;
+        })
+        .catch((error) => {
+          console.log(error);
+          debugger;
+        });
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <Header root={false} />
+          <div className="catImage">
+            <img src={this.state.imageSource} />
+          </div>
+          <div className="uploaderContent">
+            <label htmlFor="image-upload" className="customImgUpload buttonStyle">
+              UPLOAD CAT
+            </label>
+          </div>
+          <input
+            ref="file"
+            type="file"
+            id="image-upload"
+            name="imageFile"
+            accept="image/jpeg, image/png"
+            onChange={()=>this._onChange()}
+            className="defaultInput"
+          />
+          <div className="save">
+            <div
+              className={"saveBtn " + (this.state.saveButton ? 'activeSave' : 'inactiveSave')}
+              onClick={()=>this.submit()}
+              >
+              SAVE
+            </div>
+          </div>
+      </div>
+    )
+  }
+
+}
 
 const mapStateToProps = (state, props) => {
     return {
