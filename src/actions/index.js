97a479c2564b6d8587ddbd6c483caf46f990c06c
Anthea Middleton <antheamiddleton@shotscope.com>
2021-02-21 14:10:24 +0000

    Remove duplicates

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@ export const CAT_LIST = 'CAT_LIST';
 
 // Store the cat list to save having to do multiple API calls
 export function setCatList(catList) {
-    return {
-        type: 'CAT_LIST',
-        catList
-    }
+  return {
+      type: 'CAT_LIST',
+      catList
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,53 @@ class App extends Component {
     super(props);
     // Set a loading state to show a spinner while we retrieve cats
     this.state = {
-      loading: true
+      loading: true,
+      errorMsg: false
     }
   }
+
+  componentDidMount() {
+    //Retrieve cats
+    var self = this;
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'x-api-key': '45d49036-1938-44e2-b443-af805aeb55fb'}
+    };
+
+    // Retrieve the max number of 100 cats
+    fetch(`https://api.thecatapi.com/v1/images?limit=100`, requestOptions)
+        .then(response => response.json())
+        .then((responseJson)=> {
+          if (responseJson.length == undefined) {
+            //An error has occured
+            self.setState({
+              loading: false,
+              errorMsg: true
+            });
+          } else {
+            // Set these cats in the reducer
+            self.props.actions.setCatList(self.removeDuplicates(responseJson, "sub_id"));
+            self.setState({
+              loading: false
+            });
+          }
+        })
+        .catch((error) => {
+          self.setState({
+            loading: false
+          });
+          console.log(error);
+        });
+  }
+
+  //Remove duplicates from cat list
+  removeDuplicates(myArr, prop) {
+      return myArr.filter((obj, pos, arr) => {
+          return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+      });
+  }
+
+
   render() {
     return (
       <div>
@@ -25,7 +69,11 @@ class App extends Component {
         }
         <div className="container">
           <Header root />
-          Test
+          {this.state.errorMsg &&
+            <div className="catRetrievalError">
+              There has been a problem retrieving your cats. Please check your internet connection and try again
+            </div>
+          }
         </div>
       </div>
     )
